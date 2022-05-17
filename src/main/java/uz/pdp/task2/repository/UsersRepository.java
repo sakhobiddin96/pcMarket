11bf44fdce07c9784c7720caf05dea708e0b666b
sakhobiddin <nsakhobiddin@gmail.com>
2022-05-17 22:08:05 +0200

    first commit

--- src/main/java/uz/pdp/task2/repository/UsersRepository.java
+++ src/main/java/uz/pdp/task2/repository/UsersRepository.java
@@ -3,6 +3,6 @@ package uz.pdp.task2.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import uz.pdp.task2.entity.Users;
-@RepositoryRestResource(path = "users")
+
 public interface UsersRepository extends JpaRepository<Users,Integer> {
 }
